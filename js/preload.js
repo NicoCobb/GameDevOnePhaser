a18de0c1995a40ee13ac1ee27d29fd497e7e46eb
sam <zenkdare@gmail.com>
2018-10-14 17:24:35 -0400

    made two separate asset folders

--- js/preload.js
+++ js/preload.js
@@ -7,15 +7,15 @@ let preloadState = function(){
 preloadState.prototype.preload = function(){
     //game.load.image("name", "filepath")
     //game.load.spritesheet("name", "filepath", x, y)
-    game.load.image("cutting_board", "assets/cutting_board.png");
-    game.load.image("recipebook", "assets/recipebook.png");
-    game.load.image("main_background", "assets/main_background.png");
-    game.load.image("workplace", "assets/workplace.png");
-    game.load.image("dialoguebox_customer", "assets/dialoguebox_customer.png");
-    game.load.image("inventory_icon","assets/inventory_icon.png");
-    game.load.image("stirring_bowl", "assets/stirring_bowl.png");
-    game.load.image("stove","assets/stove.png");
-    game.load.image("sidebar","assets/sidebar.png");
+    game.load.image("cutting_board", "assets/art/cutting_board.png");
+    game.load.image("recipebook", "assets/art/recipebook.png");
+    game.load.image("main_background", "assets/art/main_background.png");
+    game.load.image("workplace", "assets/art/workplace.png");
+    game.load.image("dialoguebox_customer", "assets/art/dialoguebox_customer.png");
+    game.load.image("inventory_icon","assets/art/inventory_icon.png");
+    game.load.image("stirring_bowl", "assets/art/stirring_bowl.png");
+    game.load.image("stove","assets/art/stove.png");
+    game.load.image("sidebar","assets/art/sidebar.png");
 };
 
 preloadState.prototype.create = function(){
