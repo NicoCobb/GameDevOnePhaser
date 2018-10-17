3c7ca84f2143334e833f7fbf45d8737a4ccbb699
Xuan Liu <seamasliu@gmail.com>
2018-10-16 21:24:20 -0400

    Updated menu state

--- js/Menu.js
+++ js/Menu.js
@@ -4,8 +4,8 @@ let menuState = function(){
 
 menuState.prototype.create = function(){
     this.title = game.add.sprite(0, 0, "title");
-    this.play_button = game.add.button(game.world.centerX+15, game.world.centerY-105, "play_button", this.start_game, this);
-    this.exit_button = game.add.button(game.world.centerX+335, game.world.centerY+180, "exit_button", this.end_game, this);
+    this.play_button = game.add.button(game.world.centerX+40, game.world.centerY-85, "play_button", this.start_game, this);
+    this.exit_button = game.add.button(game.world.centerX+360, game.world.centerY+210, "exit_button", this.end_game, this);
 };
 
 menuState.prototype.update = function(){
