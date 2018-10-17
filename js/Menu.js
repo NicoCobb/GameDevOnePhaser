17d477611cbc48423be07cf33c91ca89c225d898
Xuan Liu <seamasliu@gmail.com>
2018-10-16 21:08:08 -0400

    Got menu working

--- js/Menu.js
+++ js/Menu.js
@@ -2,14 +2,20 @@ let menuState = function(){
 
 };
 
-preloadState.prototype.preload = function(){
+menuState.prototype.create = function(){
+    this.title = game.add.sprite(0, 0, "title");
+    this.play_button = game.add.button(game.world.centerX+15, game.world.centerY-105, "play_button", this.start_game, this);
+    this.exit_button = game.add.button(game.world.centerX+335, game.world.centerY+180, "exit_button", this.end_game, this);
+};
+
+menuState.prototype.update = function(){
 
 };
 
-preloadState.prototype.create = function(){
-
+menuState.prototype.start_game = function(){
+    game.state.start("Game");
 };
 
-preloadState.prototype.update = function(){
-
+menuState.prototype.end_game = function(){
+    game.destroy();
 };
